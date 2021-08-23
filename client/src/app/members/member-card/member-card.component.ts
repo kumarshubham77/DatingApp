bace270c584430d5c0f5f6e82a2f32304465573e
kumarshubham77 <kumarshubhamkaka@gmail.com>
2021-08-23 17:44:10 +0530

    End 9

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,19 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() member : any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
