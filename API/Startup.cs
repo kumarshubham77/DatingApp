076b46409d0f0e67dc139f735d0d4e62f1ed1f8f
Kumar Shubham <kumarshubhamkaka@gmail.com>
2023-02-07 16:36:04 +0530

    Final DEMO

--- API/Startup.cs
+++ API/Startup.cs
@@ -74,6 +74,9 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
